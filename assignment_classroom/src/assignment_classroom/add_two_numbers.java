094daa55115eff0cc8ca0a0c0eab495bba9f0849
Alver-F <rbca22108.rcca@gmail.com>
2024-01-17 10:39:25 +0530

    Commit

--- assignment_classroom/src/assignment_classroom/add_two_numbers.java
+++ assignment_classroom/src/assignment_classroom/add_two_numbers.java
@@ -5,15 +5,16 @@ public class add_two_numbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num1,num2,sum=0;
+		int num1,num2,sub=0,sum=0;
 		Scanner scan=new Scanner(System.in);
-		System.out.println("Addition:");
+		System.out.println("Subtraction:");
 		System.out.println("Enter number1:");
 		num1=scan.nextInt();
 		System.out.println("Enter number2:");
 		num2=scan.nextInt();
-		
 		sum=num1+num2;
+		sub=num1-num2;
+		System.out.println("Sub:"+sub);
 		System.out.println("Sum:"+sum);
 		
 
